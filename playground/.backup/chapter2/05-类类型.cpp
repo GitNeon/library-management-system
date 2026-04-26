5c4875baedcd98e905c7c4f0b52ba42fa344b31f
fanxine <708931190@qq.com>
2026-04-26 23:33:22 +0800

    自己实现一个Int类型的栈

---
+++ playground/.backup/chapter2/05-类类型.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+/**
+ * 自定义MyString实现
+ */
+class MyString {
+public:
+    // 一组重载的构造函数
+    // 提供自动初始化功能
+    // String str1;
+    MyString();
+    // String str2("this is mystring")
+    MyString(const char* str);
+    // String str3(str2);
+    MyString(const MyString& str);
+
+    // 析构函数
+    ~MyString();
+
+    // 一组重载的赋值操作符
+    // str1 = str2
+    MyString& operator=(const MyString& str);
+    // str3 = "a string literal"
+    MyString& operator=(const char* str);
+
+    // 一组重载的等于操作符
+    // str1 == str2;
+    // str3 == a string literal ;
+    bool operator==( const MyString& str);
+    bool operator==( const char* str);
+
+    // 重载下标操作符
+    char& operator[](int index);
+
+    const MyString operator+(const MyString& str1) const;
+
+    // 成员访问函数
+    int size() {
+        return _size;
+    }
+
+    char* c_str() {
+        return _string;
+    }
+
+private:
+    int _size;
+    char *_string;
+};
+
+// 实现两个字符串对象的比较，str1 == str2
+inline bool MyString::operator==(const MyString &rhs) {
+    if (_size != rhs._size) {
+        return false;
+    }
+    return strcmp(_string, rhs._string) ? false : true;
+}
+
+int main() {
+
+}
--- playground/test.cpp
+++ playground/test.cpp
@@ -1,61 +1,98 @@
+#include <vector>
 #include <iostream>
-#include <fstream>
-#include <cstring>
 
-/**
- * 自定义MyString实现
- */
-class MyString {
+using namespace std;
+
+class IStack {
+
 public:
-    // 一组重载的构造函数
-    // 提供自动初始化功能
-    // String str1;
-    MyString();
-    // String str2("this is mystring")
-    MyString(const char* str);
-    // String str3(str2);
-    MyString(const MyString& str);
+    IStack(int capacity): _top(0), _stack(capacity) {};
 
-    // 析构函数
-    ~MyString();
+    int size();
 
-    // 一组重载的赋值操作符
-    // str1 = str2
-    MyString& operator=(const MyString& str);
-    // str3 = "a string literal"
-    MyString& operator=(const char* str);
+    bool pop(int &value);
+    bool push(int value);
 
-    // 一组重载的等于操作符
-    // str1 == str2;
-    // str3 == a string literal ;
-    bool operator==( const MyString& str);
-    bool operator==( const char* str);
+    bool full();
+    bool isEmpty();
 
-    // 重载下标操作符
-    char& operator[](int index);
-
-    // 成员访问函数
-    int size() {
-        return _size;
-    }
-
-    char* c_str() {
-        return _string;
-    }
+    void display();
 
 private:
-    int _size;
-    char *_string;
+    int _top;   // 当前栈顶索引,_top的当前值反映了栈中元素的个数
+    vector<int> _stack;
 };
 
-// 实现两个字符串对象的比较，str1 == str2
-inline bool MyString::operator==(const MyString &rhs) {
-    if (_size != rhs._size) {
-        return false;
-    }
-    return strcmp(_string, rhs._string) ? false : true;
+inline int IStack::size() {
+    return _top;
+};
+
+inline bool IStack::isEmpty() {
+    /**
+     *  return _top ? false : true; （下乘：逻辑绕弯，过度使用三元运算符）
+        return static_cast<bool>(_top); （中乘：符合规范，意图明确，但略显啰嗦）
+        return !_top; 或 return _top == 0; （上乘：最符合直觉，极简，没有多余符号）
+     */
+    return static_cast<bool>(_top);
+};
+
+inline bool IStack::full() {
+    return static_cast<size_t>(_top) == _stack.size() - 1;
 }
 
-int main() {
+bool IStack::pop(int &top_value){
+    if(isEmpty()) {
+        return false;
+    }
 
+    top_value = _stack[--_top];
+    std::cout << "iStack::pop(): " << top_value << endl;
+
+    return true;
+}
+
+bool IStack::push(int value) {
+    if (full()) {
+        return false;
+    }
+
+    cout << "iStack::push( " << value << " )\n";
+
+    _stack[ _top++ ] = value;
+    return true;
+}
+
+void IStack::display() {
+ if ( !size() ){
+    cout << "( 0 )\n"; return;
+ }
+
+ cout << "(size =  " << size() << " )( bot: ";
+
+ for ( int ix = 0; ix < _top; ++ix ) {
+     cout << _stack[ ix ] << " ";
+ }
+
+ cout << " :top )\n";
+}
+
+int main(){
+    IStack stack( 32 );
+
+    stack.display();
+    for ( int ix = 1; ix < 51; ++ix ) {
+        if ( ix % 2 == 0 ) {
+            stack.push( ix );
+        }
+
+        if ( ix % 5 == 0 ) {
+            stack.display();
+        }
+
+        if ( ix % 10 == 0) {
+            int dummy;
+            stack.pop( dummy ); stack.pop( dummy );
+            stack.display();
+        }
+    }
 }
